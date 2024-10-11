b0973e142980a5519b07c34bd23b6ff31a7c4e50
jv291 <jv291@kent.ac.uk>
2024-10-11 10:18:18 +0000

    task 5

--- partFive.py
+++ partFive.py
@@ -1,1 +1,6 @@
-
+day = input("What day of the week is it?:")
+match day:
+    case "Monday"|"Tuesday"|"Wednesday"|"Thursday"|"Friday":
+        print("Weekday")
+    case "Saturday"|"Sunday":
+        print("Weekend")
