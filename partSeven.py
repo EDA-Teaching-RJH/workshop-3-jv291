ff57abe64de5238928b116b9e70a945f63f762e4
jv291 <jv291@kent.ac.uk>
2024-10-11 10:50:51 +0000

    seven

--- partSeven.py
+++ partSeven.py
@@ -1,1 +1,25 @@
+num1 = int(input("enter a whole number"))
+num2 = int(input("enter a whole number"))
+operand = input("enter and operation")
+match operand:
+    case "+":
+        num3= num1+num2
+        print(num3)
+    case "-":
+        num3= num1-num2
+        print(num3)
+    case "*":
+        num3= num1*num2
+        print(num3)
+    case "/":
+        num3= num1/num2
+        print(num3)
+    case "^":
+        num3=num1**num2
+        print(num3)
+    case "%":
+        num3=num1/num2
+        num4= num3*100
+        print(num4)
+        
 
