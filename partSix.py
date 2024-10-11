87445233d7b8aa0bbf5d45d42557b1b152cf0041
jv291 <jv291@kent.ac.uk>
2024-10-11 10:29:08 +0000

    tast 6

--- partSix.py
+++ partSix.py
@@ -1,1 +1,8 @@
-
+age= int(input("How old are you?:"))
+student = input("are you a student?")
+if age < 12 or age >65:
+    print("£5 per ticket")
+elif age >= 12 and age <65 and student == ("yes"):
+    print ("£8 per ticket")
+else:
+    print("£10 per ticket")
