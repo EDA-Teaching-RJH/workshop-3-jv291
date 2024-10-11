826c1549d1a1a2a2dea78c28806fdf09ad1ceeaa
jv291 <jv291@kent.ac.uk>
2024-10-11 09:52:29 +0000

    tast 2

--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,11 @@
+import random
+secret_number = random.randint(1, 10)
+guess=int(input("pick an number between 1 and 10."))
+if guess == secret_number :
+    print("correct")
+elif guess> secret_number :
+    print("to high")
+elif guess< secret_number :
+    print("too low")
+
 
