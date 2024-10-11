dc3899a60b9a889beb8e912eb0033367d424e1cb
jv291 <jv291@kent.ac.uk>
2024-10-11 10:06:50 +0000

    tast 4

--- partFour.py
+++ partFour.py
@@ -1,1 +1,9 @@
+user= str(input("enter username:"))
+password= str(input("enter password:"))
+user1= ("ADMIN")
+pass1= ("Kent")
+if user == user1 and password == pass1:
+    print("Welcome")
+else:
+    print("access denied")
 
