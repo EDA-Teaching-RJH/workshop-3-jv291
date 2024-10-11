59da51b0fc7dfec926bba9edd6cc9add674f4e39
jv291 <jv291@kent.ac.uk>
2024-10-11 09:52:36 +0000

    hu

--- partTwo.py
+++ partTwo.py
@@ -1,11 +1,9 @@
 import random
-secret_number = random.randint(1, 10)
-guess=int(input("pick an number between 1 and 10."))
-if guess == secret_number :
+secret_number= random.randint(1, 10)
+guess = int(input("Pick a number between 1 and 10:"))
+if guess == secret_number:
     print("correct")
-elif guess> secret_number :
-    print("to high")
-elif guess< secret_number :
-    print("too low")
-
-
+elif guess > secret_number:
+    print("too high")
+elif guess < secret_number:
+    print("to low")
